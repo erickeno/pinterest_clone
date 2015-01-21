d86428103aadc70fdacdfe2ffe36ab67ae4fc904
Erick Bett <erickeno@yahoo.com>
2015-01-21 12:12:54 -0500

    Implement creating tickets for a project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'boards#index'
 
-  resources :boards
+  resources :boards do
+    resources :pins
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120062905) do
+ActiveRecord::Schema.define(version: 20150121163941) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20150120062905) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.integer  "board_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "pins", ["board_id"], name: "index_pins_on_board_id"
 
 end
